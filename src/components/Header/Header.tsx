692dcbcc27c9e7d76e5afa159363bc9cfda378cc
Ritesh Narwade <riteshnarwade03@gmail.com>
2024-01-06 13:44:06 +0530

    Testing

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -2,19 +2,24 @@ import { Link, NavLink } from "react-router-dom";
 import "./Header.css";
 
 const Header = () => {
-    return (
-        <header>
-            <Link to={"/"}>Template-Hub</Link>
-            <nav>
-                <ul>
-                    <NavLink to={"/"}>home</NavLink>
-                    <NavLink to={"/"}>about us</NavLink>
-                    <NavLink to={"/"}>contact</NavLink>
-                    <NavLink to={"/"}>login</NavLink>
-                </ul>
-            </nav>
-        </header>
-    );
+  return (
+    <header>
+      <Link to={"/"}>Template-Hub</Link>
+      <nav>
+        <ul>
+          <NavLink to={"/"}>home</NavLink>
+          <NavLink to={"/"}>about us</NavLink>
+          <NavLink to={"/"}>contact</NavLink>
+          <NavLink to={"/"}>login</NavLink>
+          {/* NEW */}
+          <NavLink to={"/"}>home</NavLink>
+          <NavLink to={"/"}>about us</NavLink>
+          <NavLink to={"/"}>contact</NavLink>
+          <NavLink to={"/"}>login</NavLink>
+        </ul>
+      </nav>
+    </header>
+  );
 };
 
 export default Header;
