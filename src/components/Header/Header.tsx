7338eff02b70b45bb5390631b52bb1b2e51ca4d4
Ritesh Narwade <riteshnarwade03@gmail.com>
2024-01-06 20:24:41 +0530

    Working on Navbar

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,24 +1,59 @@
 import { Link, NavLink } from "react-router-dom";
-import "./Header.css";
+import { TiShoppingCart } from "react-icons/ti";
+
+// import "./Header.css";
+
+// const Header = () => {
+//   return (
+//     <header className="flex justify-between">
+//       <nav>
+//         <ul>
+//           <Link to={"/"}>Template-Hub</Link>
+//         </ul>
+//         <ul>
+//           <NavLink className="bg-red-700" to={"/"}>
+//             <h1 className="bg-red-700">Home</h1>
+//           </NavLink>
+//           <NavLink to={"/"}>About us</NavLink>
+//           <NavLink to={"/"}>Contact</NavLink>
+//           <NavLink to={"/"}>Login</NavLink>
+//         </ul>
+//       </nav>
+//     </header>
+//   );
+// };
 
 const Header = () => {
   return (
-    <header>
-      <Link to={"/"}>Template-Hub</Link>
-      <nav>
-        <ul>
-          <NavLink to={"/"}>home</NavLink>
-          <NavLink to={"/"}>about us</NavLink>
-          <NavLink to={"/"}>contact</NavLink>
-          <NavLink to={"/"}>login</NavLink>
-          {/* NEW */}
-          <NavLink to={"/"}>home</NavLink>
-          <NavLink to={"/"}>about us</NavLink>
-          <NavLink to={"/"}>contact</NavLink>
-          <NavLink to={"/"}>login</NavLink>
-        </ul>
-      </nav>
-    </header>
+    <>
+      <header className="flex justify-between bg-light-red px-8 py-6 text-red-600">
+        <div>
+          <Link to={"/"}>
+            <h1>Template Hub</h1>
+          </Link>
+        </div>
+        <div className="flex justify-between gap-10 ">
+          <NavLink to={"/"}>
+            <h3 className="">Home</h3>
+          </NavLink>
+          <NavLink to={"/"}>
+            <h3 className="">About Us</h3>
+          </NavLink>
+          <NavLink to={"/"}>
+            <h3 className=""> Contact</h3>
+          </NavLink>
+          <NavLink to={"/"}>
+            <h3 className="">Login</h3>
+          </NavLink>
+          <NavLink to={"/"}>
+            <button className="border-2 px-10 flex items-center rounded">
+              <TiShoppingCart />
+              Sign In
+            </button>
+          </NavLink>
+        </div>
+      </header>
+    </>
   );
 };
 
