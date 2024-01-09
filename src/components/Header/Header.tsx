94c6484450085387001dbac8a9c2005ceb0000c2
Ritesh Narwade <riteshnarwade03@gmail.com>
2024-01-09 22:15:26 +0530

    Added The Burger Button

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,56 +1,93 @@
 import { Link, NavLink } from "react-router-dom";
 import { TiShoppingCart } from "react-icons/ti";
+import { RiCloseLine, RiMenu3Line } from "react-icons/ri";
+import { useState } from "react";
 
-// import "./Header.css";
+// nav is block ele
+//
 
-// const Header = () => {
-//   return (
-//     <header className="flex justify-between">
-//       <nav>
-//         <ul>
-//           <Link to={"/"}>Template-Hub</Link>
-//         </ul>
-//         <ul>
-//           <NavLink className="bg-red-700" to={"/"}>
-//             <h1 className="bg-red-700">Home</h1>
-//           </NavLink>
-//           <NavLink to={"/"}>About us</NavLink>
-//           <NavLink to={"/"}>Contact</NavLink>
-//           <NavLink to={"/"}>Login</NavLink>
-//         </ul>
-//       </nav>
-//     </header>
-//   );
-// };
-
-const Header = () => {
+const Menu = () => {
   return (
     <>
-      <header className="flex justify-between bg-light-red px-8 py-6 text-red-600">
-        <div>
-          <Link to={"/"}>
-            <h1>Template Hub</h1>
-          </Link>
-        </div>
-        <div className="flex justify-between gap-10 ">
-          <NavLink to={"/"}>
-            <h3 className="">Home</h3>
-          </NavLink>
-          <NavLink to={"/"}>
-            <h3 className="">About Us</h3>
-          </NavLink>
-          <NavLink to={"/"}>
-            <h3 className=""> Contact</h3>
-          </NavLink>
-          <NavLink to={"/"}>
-            <h3 className="">Login</h3>
-          </NavLink>
-          <NavLink to={"/"}>
-            <button className="border-2 px-10 flex items-center rounded">
+      <nav>
+        <ul>
+          <li>
+            <NavLink to={"/"}>Home</NavLink>
+          </li>
+          <li>
+            <NavLink to={"/"}>About Us</NavLink>
+          </li>
+          <li>
+            <NavLink to={"/"}>Contact</NavLink>
+          </li>
+          <li>
+            <NavLink to={"dashboard/login"}>Login</NavLink>
+          </li>
+          <li className="sm:hidden">
+            <button className="border-2 px-6 py-2 flex items-center rounded gap-2">
               <TiShoppingCart />
-              Sign In
+              <h3>Sign Up</h3>
             </button>
-          </NavLink>
+          </li>
+        </ul>
+      </nav>
+    </>
+  );
+};
+
+const Header = () => {
+  const [toggleMenu, setToggleMenu] = useState(false);
+  return (
+    <>
+      <header className="flex justify-between items-center bg-light-red px-8 py-6 text-red-600">
+        <div>
+          <Link to={"/"}>Template Hub</Link>
+        </div>
+        <div>
+          <nav className="flex justify-between gap-10 items-center sm:hidden md:flex lg:flex xl:flex 2xl:flex">
+            <ul className="flex gap-10 items-center">
+              <li>
+                <NavLink to={"/"}>Home</NavLink>
+              </li>
+              <li>
+                <NavLink to={"/"}>About Us</NavLink>
+              </li>
+              <li>
+                <NavLink to={"/"}>Contact</NavLink>
+              </li>
+              <li>
+                <NavLink to={"dashboard/login"}>Login</NavLink>
+              </li>
+              <li>
+                <button className="border-2 px-6 py-2 flex items-center rounded gap-2">
+                  <TiShoppingCart />
+                  <h3>Sign Up</h3>
+                </button>
+              </li>
+            </ul>
+          </nav>
+        </div>
+        <div className="flex items-center  md:hidden lg:hidden xl:hidden 2xl:hidden">
+          <div>
+            <Link to={"/"}>
+              <button className="border-2 px-6 py-2 flex items-center rounded gap-2 sm:p-1 mx-6">
+                <TiShoppingCart />
+                <h3>Sign Up</h3>
+              </button>
+            </Link>
+          </div>
+          {toggleMenu ? (
+            <RiCloseLine onClick={() => setToggleMenu(false)} />
+          ) : (
+            <RiMenu3Line onClick={() => setToggleMenu(true)} />
+          )}
+          {toggleMenu && (
+            <span className="flex gap-4 md:hidden lg:hidden xl:hidden 2xl:hidden">
+              <div className="flex justify-end absolute items-end flex-col text-end p-6 top-10 right-0 mt-8  bg-light-red rounded">
+                <Menu />
+              </div>
+            </span>
+          )}
         </div>
       </header>
     </>
--- src/pages/DashPage/Login/Login.tsx
+++ src/pages/DashPage/Login/Login.tsx
@@ -6,61 +6,62 @@ import { RootState } from "../../../store/rootReducer";
 import { apiUrl } from "../../../lib/constant";
 
 const Login = () => {
-    const { email, password, setEmail, setPassword, handleLogin } =
-        useLogin(apiUrl);
+  const { email, password, setEmail, setPassword, handleLogin } =
+    useLogin(apiUrl);
 
-    const isAuthenticated = useSelector(
-        (state: RootState) => state.auth.isAuthenticated
-    );
+  const isAuthenticated = useSelector(
+    (state: RootState) => state.auth.isAuthenticated
+  );
 
-    if (isAuthenticated) return <Navigate to={"/dashboard"} />;
+  if (isAuthenticated) return <Navigate to={"/dashboard"} />;
 
-    return (
-        <form id="sign_in">
-            <h2>Sign in</h2>
-            {/* <!--Email and password input are contained here and within--> */}
-            <div className="inputbx">
-                <div className="email">
-                    <input
-                        id="email"
-                        type="email"
-                        autoComplete="true"
-                        required
-                        onChange={(e) => setEmail(e.target.value)}
-                    />
-                    <label htmlFor="email">Enter email</label>
-                </div>
-                <div className="password">
-                    <input
-                        id="password"
-                        type="password"
-                        required
-                        onChange={(e) => setPassword(e.target.value)}
-                    />
-                    <label htmlFor="password">Enter password</label>
-                </div>
-            </div>
+  return (
+    <form id="sign_in">
+      <h2>Sign in</h2>
+      {/* <!--Email and password input are contained here and within--> */}
+      <div className="inputbx">
+        <div className="email">
+          <input
+            id="email"
+            type="email"
+            autoComplete="true"
+            required
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <label htmlFor="email">Enter email</label>
+        </div>
+        <div className="password">
+          <input
+            id="password"
+            type="password"
+            required
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <label htmlFor="password">Enter password</label>
+        </div>
+      </div>
 
-            {/* <!--Call to action container--> */}
-            <div className="btnbx">
-                <button
-                    type="submit"
-                    disabled={!email || !password}
-                    onClick={(e) => {
-                        e.preventDefault();
-                        handleLogin();
-                    }}>
-                    Sign in
-                </button>
+      {/* <!--Call to action container--> */}
+      <div className="btnbx">
+        <button
+          type="submit"
+          disabled={!email || !password}
+          onClick={(e) => {
+            e.preventDefault();
+            handleLogin();
+          }}
+        >
+          Sign in
+        </button>
 
-                <p>
-                    Don't have an account,
-                    <Link to={"/dashboard/register"}>Register</Link>
-                    here
-                </p>
-            </div>
-        </form>
-    );
+        <p>
+          Don't have an account,
+          <Link to={"/dashboard/register"}>Register</Link>
+          here
+        </p>
+      </div>
+    </form>
+  );
 };
 
 export default Login;
